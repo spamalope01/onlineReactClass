c838a3344e397a84dfb33be485dd6bdae371ca70
Jonathan Daniel (Unify Consulting) <Jonathan.Daniel@gatesfoundation.org>
2019-02-19 09:29:39 -0800

    small push

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker';
 
+class StorePicker extends React.Component {
+  render () {
+    return <p>Hello!</p>
+  }
+}
+
+
+
 render(<StorePicker />, document.querySelector('#main'));
